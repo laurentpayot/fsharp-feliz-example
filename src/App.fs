40398dc419447c54e6f35e19dc41c18f2a5c84fa
Laurent <laurent.payot@publiberty.com>
2024-01-31 22:37:19 +0100

    Elmish

---
+++ src/App.fs
@@ -0,0 +1,26 @@
+module App
+
+open Feliz
+open Elmish
+
+type Model = { count: int }
+
+type Msg =
+    | Increment
+    | Decrement
+
+let init () = { count = 0 }, Cmd.none
+
+let update (msg: Msg) (model: Model) =
+    match msg with
+    | Increment -> { model with count = model.count + 1 }, Cmd.none
+    | Decrement -> { model with count = model.count - 1 }, Cmd.none
+
+let view (model: Model) (dispatch: Msg -> unit) =
+    Html.div [
+        Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "Increment" ]
+
+        Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "Decrement" ]
+
+        Html.h1 model.count
+    ]
--- src/Main.fs
+++ src/Main.fs
@@ -1,8 +1,12 @@
 module Main
 
-open Feliz
 open App
-open Browser.Dom
+open Elmish
 
-let root = ReactDOM.createRoot(document.getElementById "feliz-app")
-root.render(Components.Counter())
+#if DEBUG
+open Elmish.HMR // must be after all the other Elmish imports
+#endif
+
+Program.mkProgram init update view
+|> Program.withReactSynchronous "feliz-app"
+|> Program.run
